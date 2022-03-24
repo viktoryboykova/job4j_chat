c9209be30a40be9b06247f32423206d4c989545e
viktoryboykova <viktoryboykova@list.ru>
2022-03-24 14:02:21 +0300

    create job4j_chat

---
+++ src/main/java/ru/job4j/Job4jChatApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jChatApplication.class, args);
+    }
+
+}
